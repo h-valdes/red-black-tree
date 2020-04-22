9c109248411c047c24990c664756946a09929947
Hernán Valdés <hfvaldesg@gmail.com>
2020-04-22 11:55:10 +0200

    Add more prototype functions and refactor the name of the tree

--- include/rbtree.h
+++ include/rbtree.h
@@ -9,13 +9,20 @@ typedef struct Node {
     enum color_t color;
     int key;
     size_t size;
+    char *data;
     struct Node *parent_node;
     struct Node *left_node;
     struct Node *right_node;
-} Node;
+} Node_t;
 
-typedef struct Tree {
+typedef struct RBT_t {
     struct Node *root_node;
-} Tree;
+} RBT_t;
+
+RBT_t *newRBT();
+
+_Bool RBT_insert();
+
+void RBT_clear(RBT_t *pRBT);
 
 #endif
