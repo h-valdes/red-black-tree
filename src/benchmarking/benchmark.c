e9a8ceb2e2f551117589747556e7f4bd14d6a4a8
Hernán Valdés <hfvaldesg@gmail.com>
2020-05-20 14:09:42 +0200

    Update

--- src/benchmarking/benchmark.c
+++ src/benchmarking/benchmark.c
@@ -4,35 +4,66 @@
 #include <time.h>
 
 #include "benchmarking/binary_search_tree.h"
+#include "benchmarking/red_black_tree.h"
+#include "benchmarking/colors.h"
 #include "benchmarking/comparators.h"
 #include "benchmarking/debug.h"
-#include "rbtree.h"
 #include "benchmarking/benchmark.h"
 
-void visit(node* n) {
+#include "rbtree.h"
+
+void visit_bst(node* n) {
     // printf("Visited node #%d\n",*(int*)n->data);
+}
+
+void visit_rbt(node* n)
+{
+    DBG("Visited node #%d (%c) \tPARENT=%d (%c)\tHEIGHT=%d, COLOR=%d\n",
+    *(int*)n->data,
+    *(int*)n->data,
+    n->parent?*(int*)n->parent->data:-1,
+    n->parent?*(int*)n->parent->data:'*',
+    height(n),
+    n->color
+    );
 }
 
 void benchmark() {
     printf("\nSTART BENCHMARKING\n");
-    int count = 1000;
+
+    // Generate 10000 elements in order and
+    int count = 10000;
     int values[count];
     for (int i = 1; i < count; i++) {
         values[i] = i;
     }
 
+    printf("\tInsertion of %d ordered/ascendent integers\n", count);
+
     // Insert RB Tree
 
     clock_t start = clock();
 
-    RBT_t* intTree = RBT_new_int();
+    RBT_t* rbTree = RBT_new_int();
     for (int i = 1; i < count; i++) {
-        RBT_insert(intTree, values[i], &values[i], sizeof(values[i]));
+        RBT_insert(rbTree, values[i], &values[i], sizeof(values[i]));
     }
 
-    printf("Insertion Time RBT: %.6fms\n", (double)(clock() - start) / (CLOCKS_PER_SEC / 1000));
+    printf("\tInsertion Time RBT: %.6fms\n", (double)(clock() - start) / (CLOCKS_PER_SEC / 1000));
 
-    RBT_clear_tree(intTree);
+    RBT_clear_tree(rbTree);
+
+    // Insert alternative RBT (cstuff)
+    start = clock();
+
+    binary_tree* rbt = new_binary_tree(compare_integer, ORD_ASC);
+    for (int i = 1; i < count; i++) {
+        int* data = malloc(sizeof(int));
+        *data = values[i];
+        rb_insert(rbt, (void*)data, TRUE);
+    }
+
+    printf("\tInsertion Time RBT (cstuff library): %.6fms\n", (double)(clock() - start) / (CLOCKS_PER_SEC / 1000));
 
     // Insert BST
 
@@ -46,8 +77,10 @@ void benchmark() {
         *data = i;
         node* n = tree_insert(bt, data, TRUE);
     }
-    printf("Insertion Time BST: %.6fms\n", (double)(clock() - start) / (CLOCKS_PER_SEC / 1000));
+    printf("\tInsertion Time BST (cstuff library): %.6fms\n", (double)(clock() - start) / (CLOCKS_PER_SEC / 1000));
 
-    depth_first(bt, visit, IN_ORDER);
+    
+
+    
 
 }
--- src/main.c
+++ src/main.c
@@ -6,18 +6,18 @@
 #include "benchmarking/benchmark.h"
 
 int main(int argc, char* argv[]) {
-    int count = 10;
-    int values[count];
-    for (int i = 1; i < count; i++) {
-        values[i] = i;
-    }
+    // int count = 1000;
+    // int values[count];
+    // for (int i = 1; i < count; i++) {
+    //     values[i] = i;
+    // }
 
-    RBT_t* intTree = RBT_new_int();
-    for (int i = 1; i < count; i++) {
-        RBT_insert(intTree, values[i], &values[i], sizeof(values[i]));
-    }
+    // RBT_t* intTree = RBT_new_int();
+    // for (int i = 1; i < count; i++) {
+    //     RBT_insert(intTree, values[i], &values[i], sizeof(values[i]));
+    // }
 
-    RBT_clear_tree(intTree);
+    // RBT_clear_tree(intTree);
 
     benchmark();
 
