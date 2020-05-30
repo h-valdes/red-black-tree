f65b35dad8f588b46a36f43bf14ec6724cf69b16
Hernán Valdés <hfvaldesg@gmail.com>
2020-05-30 20:01:32 +0200

    Add new graphs excluding the BST for major visibility

--- benchmark/plot/plot.py
+++ benchmark/plot/plot.py
@@ -8,6 +8,8 @@ search_csv = pd.read_csv("search-1-100000.csv", header=None)
 insert_csv.columns = ["name", "nodes", "avg_case", "worst_case"]
 search_csv.columns = ["name", "nodes", "avg_case", "worst_case"]
 
+# Average time
+
 insert_avg = insert_csv.pivot(index="nodes", columns="name", values="avg_case")
 fig = insert_avg.plot(style='.-').get_figure()
 fig.savefig("insert_avg.png")
@@ -16,6 +18,8 @@ search_avg = search_csv.pivot(index="nodes", columns="name", values="avg_case")
 fig = search_avg.plot(style='.-').get_figure()
 fig.savefig("search_avg.png")
 
+# Worst time
+
 insert_worst = insert_csv.pivot(index="nodes", columns="name", values="worst_case")
 fig = insert_worst.plot(style='.-').get_figure()
 fig.savefig("insert_worst.png")
@@ -23,3 +27,13 @@ fig.savefig("insert_worst.png")
 search_worst = search_csv.pivot(index="nodes", columns="name", values="worst_case")
 fig = search_worst.plot(style='.-').get_figure()
 fig.savefig("search_worst.png")
+
+# Exclude the BST from search graph
+
+search_avg_filtered = search_avg.drop(columns="BST")
+fig = search_avg_filtered.plot(style='.-').get_figure()
+fig.savefig("search_avg_filtered.png")
+
+search_worst_filtered = search_worst.drop(columns="BST")
+fig = search_worst_filtered.plot(style='.-').get_figure()
+fig.savefig("search_worst_filtered.png")
